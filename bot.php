01f2cfc1942fcdc277d046e961d3697c4097854e
pwa5 <yuppares.b@gmail.com>
2016-11-02 13:23:05 +0700

    aa

--- bot.php
+++ bot.php
@@ -49,30 +49,33 @@ if (!is_null($events['events'])) {
 }
 else
 {
-
-	//$replytext="ตอบคุณ ".$sourceInfo['displayName']."\n";
-	$pushtext="test";
-	$messages = [
-					'type' => 'text',
-					'text' =>  $pushtext
-				];
-	// Make a POST Request to Messaging API to reply to sender
-	$url = 'https://api.line.me/v2/bot/message/push';
-	$data = [
-		'to' => 'C5284c7dfc515bb45cbb338ae3be11356',
-		'messages' => [$messages],
-	];
-	$post = json_encode($data);
-	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$result = curl_exec($ch);
-	curl_close($ch);
-	echo $result . "\r\n";
+	$job=$_GET["job"];
+	if($job=="job01")
+	{
+		//$replytext="ตอบคุณ ".$sourceInfo['displayName']."\n";
+		$pushtext="test";
+		$messages = [
+						'type' => 'text',
+						'text' =>  $pushtext
+					];
+		// Make a POST Request to Messaging API to reply to sender
+		$url = 'https://api.line.me/v2/bot/message/push';
+		$data = [
+			'to' => 'C5284c7dfc515bb45cbb338ae3be11356',
+			'messages' => [$messages],
+		];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		echo $result . "\r\n";
+	}
 }
 echo "OK";
 
